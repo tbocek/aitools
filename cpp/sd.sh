b23c64d7ecf724f122960c9f35f8123630af2f17
Thomas Bocek <tom@tomp2p.net>
2025-08-01 16:08:45 +0200

    fix sd

--- cpp/sd.sh
+++ cpp/sd.sh
@@ -2,13 +2,14 @@
 set -e
 
 docker build . -t cpp
+#Multi GPU does not work well, so only expose one card
 docker run -it \
     --device=/dev/kfd \
-    --device=/dev/dri \
+    --device=/dev/dri/card1 \
+    --device=/dev/dri/renderD128 \
     --group-add render \
     --group-add video \
     --security-opt seccomp=unconfined \
-    -e CUDA_VISIBLE_DEVICES=0 \
     -p 7860:7860 \
     -v /mnt/models/sd:/home/cpp/sd.cpp-webui/models \
     cpp:latest /home/cpp/sd.cpp-webui/sdcpp_webui.sh --listen
