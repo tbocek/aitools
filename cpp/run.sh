a48b7430c28c1a612aaec8bb60710cf28394c716
Thomas Bocek <tom@tomp2p.net>
2025-09-22 21:08:30 +0200

    Update run.sh

--- cpp/run.sh
+++ cpp/run.sh
@@ -155,7 +155,7 @@ docker build . -f Dockerfile.arch -t arch
 
 msg "${GREEN}Starting services: ${SERVICES[*]}${NOFORMAT}"
 # Prepare docker-compose command
-compose_args=("up" "--build")
+compose_args=("up" "--build" "--no-cache")
 if [[ "$DETACHED" == true ]]; then
   compose_args+=("-d")
 else
