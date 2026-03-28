fcc4eb64cd8deb5acceff9cad0d8fcd63b867994
Thomas Bocek <tom@tomp2p.net>
2026-03-28 12:27:06 +0100

    improved setup

--- cpp/run.sh
+++ cpp/run.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-BACKEND="${1:-vulkan}"
-
-if [[ "$BACKEND" != "vulkan" && "$BACKEND" != "rocm" ]]; then
-  echo "Usage: $(basename "$0") [vulkan|rocm]"
-  exit 1
-fi
-
-export LLAMA_BACKEND="$BACKEND"
-export SD_BACKEND="$BACKEND"
-export ACE_BACKEND="$BACKEND"
+# ROCM0 for HIP/ROCm, Vulkan0 for Vulkan
+export LLAMA_DEVICE=Vulkan0
+# rocm or vulkan
+export SD_BACKEND=vulkan
+export ACE_BACKEND=vulkan
 
 docker build . -f Dockerfile.arch -t arch
 docker-compose up --build
