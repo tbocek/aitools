7a422b4f992a1f05d16d7ee9e66ab548c8568c89
Thomas Bocek <tom@tomp2p.net>
2026-04-03 08:59:27 +0200

    Update run.sh

--- cpp/run.sh
+++ cpp/run.sh
@@ -4,7 +4,7 @@ set -Eeuo pipefail
 # ROCM0 for HIP/ROCm, Vulkan0 for Vulkan
 export LLAMA_DEVICE=Vulkan0
 # rocm or vulkan
-export SD_BACKEND=vulkan
+export SD_BACKEND=rocm
 export ACE_BACKEND=vulkan
 
 docker build . -f Dockerfile.arch -t arch
